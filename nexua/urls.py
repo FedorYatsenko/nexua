dc38ab88046fa09c2eb53dc5783abbc3fcebcfe6
FedorYatsenko <f.yatsenko@knu.ua>
2018-03-16 12:51:42 +0200

    Add urls in file nexua/nexua/urls.py

--- nexua/urls.py
+++ nexua/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.conf.urls import url
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    url(r'^Site/', include('Site.urls')),
+    url(r'^$', RedirectView.as_view(url='/Site/', permanent=True)),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
