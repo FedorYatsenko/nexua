cc5e6f3609c2e23b3ab118c89c660a0f68dcc9b5
lockystocky <locky.stocky24@gmail.com>
2018-03-22 22:57:49 +0200

    file model functions added

--- Site/models.py
+++ Site/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
+from django.urls import reverse
 
 
 # Create your models here.
@@ -12,7 +13,13 @@ class File(models.Model):
     link = models.CharField(max_length=50, verbose_name="Link to file")
     path_on_disk = models.CharField(max_length=50, verbose_name="Path on disk")
     time_to_live = models.DurationField()
-    file_name = models.CharField(max_length=50, help_text="Enter file name", verbose_name="File type")
+    file_type = models.CharField(max_length=50, help_text="Enter file type", verbose_name="File type")
     size = models.AutoField()
     user = models.ForeignKey(User, unique=True)
+
+    def __str__(self):
+        return 'id: %s  name: %s date: %s type: %s size: %s user: %s' % (self.id, self.file_name, self.upload_date, self.file_type, self.size, self.user.__str__())
+
+    def get_absolute_url(self):
+        return reverse('file-details', args=[str(self.id)])
 
