28471b47118c8440f2a1f61bb2293a972761cd59
lockystocky <locky.stocky24@gmail.com>
2018-03-22 22:13:39 +0200

    file model added

--- Site/models.py
+++ Site/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
+
 
 # Create your models here.
+
+class File(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4())
+    file_name = models.CharField(max_length=50, help_text="Enter file name", verbose_name="File name")
+    upload_date = models.DateTimeField(verbose_name="Date of file upload")
+    link = models.CharField(max_length=50, verbose_name="Link to file")
+    path_on_disk = models.CharField(max_length=50, verbose_name="Path on disk")
+    time_to_live = models.DurationField()
+    file_name = models.CharField(max_length=50, help_text="Enter file name", verbose_name="File type")
+    size = models.AutoField()
+    user = models.ForeignKey(User, unique=True)
+
