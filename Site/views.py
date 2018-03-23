0161fa01148cc3629348c1594de958492e96d0d0
lockystocky <locky.stocky24@gmail.com>
2018-03-23 15:33:08 +0200

    upload function added

--- Site/views.py
+++ Site/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+
 
 
 def index(request):
@@ -7,3 +9,26 @@ def index(request):
         'index.html',
         context={},
     )
+
+
+def upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'upload.html', )
+    return render(
+        request,
+        'userpage/upload.html',
+        context={},
+    )
+
+# if request.method == 'POST' and request.FILES['myfile']:
+    #     myfile = request.FILES['myfile']
+    #     fs = FileSystemStorage()
+    #     filename = fs.save(myfile.name, myfile)
+    #     uploaded_file_url = fs.url(filename)
+    #     return render(request, 'upload.html', {
+    #         'uploaded_file_url': uploaded_file_url
+    #     })
