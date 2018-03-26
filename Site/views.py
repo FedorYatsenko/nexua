d38956d18e7847f997dca372fdfd007b642c4dfa
FedorYatsenko <f.yatsenko@knu.ua>
2018-03-26 22:25:16 +0300

    Add ListView class for render my_files.html in nexua/Site/views.html

--- Site/views.py
+++ Site/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import File
 
@@ -11,6 +12,7 @@ def index(request):
         context={},
     )
 
+
 def file_detail_view(request, pk):
     file_id = File.objects.get(pk=pk)
 
@@ -19,3 +21,11 @@ def file_detail_view(request, pk):
         'common/download.html',
         context={'file': file_id, }
     )
+
+class MyFilesListView(LoginRequiredMixin, generic.ListView):
+    model = File
+    paginate_by = 2
+    template_name = 'userpage/my_files.html'
+
+    def get_queryset(self):
+        return File.objects.filter(user=self.request.user)
