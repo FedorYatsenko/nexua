ccf4681f9b69fcdeb2f96199ed65f44a3a139d0d
FedorYatsenko <f.yatsenko@knu.ua>
2018-03-26 22:26:47 +0300

    Add urls for my_file.html in nexua/Site/urls.py

--- Site/urls.py
+++ Site/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
+from django.urls import path
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
 from . import views
 
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^my_files$', views.MyFilesListView.as_view(), name='my_files'),
     url(r'^file/(?P<pk>\d+)$', views.file_detail_view, name='file-detail'),
 ]
