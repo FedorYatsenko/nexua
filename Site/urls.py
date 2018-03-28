1e8b6a4c6d2c210bcc948f6442e9ef10347a2da7
FedorYatsenko <f.yatsenko@knu.ua>
2018-03-28 21:24:00 +0300

    Add url to user profile page in nexua/Site/urls.py

--- Site/urls.py
+++ Site/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^my_files$', views.MyFilesListView.as_view(), name='my_files'),
+    url(r'^profile$', views.profile, name='profile'),
     url(r'^file/(?P<pk>\d+)$', views.file_detail_view, name='file-detail'),
 ]
