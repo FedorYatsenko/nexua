bf40f18ae7583b34c367996096521d816f19e556
FedorYatsenko <f.yatsenko@knu.ua>
2018-03-30 16:49:52 +0300

    Add url to random files page in nexua/Site/urls.py

--- Site/urls.py
+++ Site/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^my_files$', views.MyFilesListView.as_view(), name='my_files'),
+    url(r'^random_files$', views.RandomFilesListView.as_view(), name='random_files'),
     url(r'^profile$', views.profile, name='profile'),
     url(r'^file/(?P<pk>\d+)$', views.file_detail_view, name='file-detail'),
 ]
