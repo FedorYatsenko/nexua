d989639bd291a91e3876b7ebca9ea4901a4cb65c
FedorYatsenko <f.yatsenko@knu.ua>
2018-03-31 21:13:53 +0300

    Add new functions for File in nexua/Site/models.py

--- Site/models.py
+++ Site/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-import uuid
+# import uuid
 from django.urls import reverse
+
+from django.utils import timezone
+
+from datetime import datetime
 
 
 # Create your models here.
@@ -22,3 +26,59 @@ class File(models.Model):
 
     def get_absolute_url(self):
         return reverse('file-detail', args=[str(self.id)])
+
+    def get_size(self):
+        if self.size // 1073741824:
+            return '{:01.2f} Gb'.format(self.size / 1073741824)
+        elif self.size // 1048576:
+            return '{:01.2f} Mb'.format(self.size / 1048576)
+        elif self.size // 1024:
+            return '{:01.2f} Kb'.format(self.size / 1024)
+        else:
+            return '{:d} Bytes'.format(self.size)
+
+    def get_time_to_delete(self):
+        if self.time_to_live:
+            period = self.time_to_live - datetime.now(timezone.now().tzinfo)
+
+            if period.days < 0:
+                return "now"
+
+            if period.days == 0:
+                if period.seconds > 3600:
+                    return '{:d} hours'.format(period.seconds // 3600)
+                elif period.seconds > 60:
+                    return '{:d} minutes'.format(period.seconds // 60)
+                else:
+                    return '{:d} seconds'.format(period.seconds)
+            else:
+                if period.days > 364:
+                    return '{:d} years'.format(period.days // 364)
+                elif period.days > 14:
+                    return '{:d} weeks'.format(period.days // 7)
+                else:
+                    return '{:d} days'.format(period.days)
+
+    def get_upload_time(self):
+        period = datetime.now(timezone.now().tzinfo) - self.upload_date
+
+        if period.days < 0:
+            return "now"
+
+        if period.days == 0:
+            if period.seconds > 3600:
+                return '{:d} hours'.format(period.seconds // 3600)
+            elif period.seconds > 60:
+                return '{:d} minutes'.format(period.seconds // 60)
+            else:
+                return '{:d} seconds'.format(period.seconds)
+        else:
+            if period.days > 364:
+                return '{:d} years'.format(period.days // 364)
+            elif period.days > 14:
+                return '{:d} weeks'.format(period.days // 7)
+            else:
+                return '{:d} days'.format(period.days)
+
+    def get_logo(self):
+        return 'img/icons/{}.png'.format(self.file_type)
