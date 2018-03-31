f1d39127c98fd8ebc116858c389431ac2e9a8874
lockystocky <locky.stocky24@gmail.com>
2018-03-31 11:44:44 +0300

    Merge branch 'master' of https://github.com/FedorYatsenko/nexua
    # Conflicts:
    #	Site/urls.py
    #	Site/views.py

--- Site/views.py
+++ Site/views.py
@@ -3,6 +3,11 @@ from django.views import generic
 
 from .models import File
 from django.core.files.storage import FileSystemStorage
+from .forms import UploadFileForm
+import datetime
+import os
+from django.core.files.storage import default_storage
+from django.conf import settings
 
 
 
@@ -12,6 +17,7 @@ def index(request):
         'index.html',
         context={},
     )
+
 
 def file_detail_view(request, pk):
     file_id = File.objects.get(pk=pk)
@@ -23,13 +29,35 @@ def file_detail_view(request, pk):
     )
 
 
+def handle_uploaded_file(request):
+    save_path = os.path.join(settings.MEDIA_ROOT, 'uploads', request.FILES.get('file'))
+    path = default_storage.save(save_path, request.FILES.get('file'))
+
+
 def upload(request):
-    if request.method == 'POST' and request.FILES['myfile']:
-        myfile = request.FILES['myfile']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
-        return render(request, 'upload.html', )
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        uploaded_file = File()
+        # uploaded_file.file_itself = form.cleaned_data.get('file')
+        uploaded_file.file_itself = request.POST.get('file')
+        uploaded_file.upload_date = datetime.datetime.now()
+        # uploaded_file.size = form.cleaned_data.get('file').size
+        uploaded_file.size = 100
+        uploaded_file.path_on_disk = 'D:\\nexua_files'
+        uploaded_file.link = 'abclink'
+        uploaded_file.file_type = 'txt'
+        uploaded_file.file_name = 'newfile1'
+        uploaded_file.time_to_live = datetime.timedelta(days=2)
+        uploaded_file.user = request.user
+        uploaded_file.save()
+        handle_uploaded_file(request.FILES.get('file'))
+
+
+
+        return render(
+            request,
+            'userpage/upload.html',
+            context={},)
     return render(
         request,
         'userpage/upload.html',
