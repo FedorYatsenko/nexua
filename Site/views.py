9faa5dbb22359a8353142e4d60f45d9956050080
FedorYatsenko <f.yatsenko@knu.ua>
2018-04-03 20:12:27 +0300

    Change index render in nexua/Site/view.py

--- Site/views.py
+++ Site/views.py
@@ -10,21 +10,64 @@ from django.utils import timezone
 from datetime import datetime, timedelta
 from django.db.models import Q
 
-from .models import File
+from .models import File, User
 from django.core.files.storage import FileSystemStorage
 from .forms import UploadFileForm
 import datetime
 import os
 from django.core.files.storage import default_storage
 from django.conf import settings
-
+from django.db.models import Sum
 
 
 def index(request):
+    files_count = File.objects.all().count()
+    available_files_count = File.objects.filter(Q(time_to_live__isnull=True) | Q(time_to_live__gt=timezone.now())).count()
+    user_count = User.objects.all().count()
+    dc_total_file_size = File.objects.all().aggregate(total_file_size=Sum('size'))
+
+    time_hour = timezone.now() + timedelta(hours=1)
+    files_count_hour = File.objects.filter(time_to_live__isnull=False,
+                                           time_to_live__gt=timezone.now(),
+                                           time_to_live__lt=time_hour
+                                           ).count()
+    time_day = timezone.now() + timedelta(days=1)
+    files_count_day = File.objects.filter(time_to_live__isnull=False,
+                                          time_to_live__gt=timezone.now(),
+                                          time_to_live__lt=time_day
+                                          ).count()
+    time_week = timezone.now() + timedelta(weeks=1)
+    files_count_week = File.objects.filter(time_to_live__isnull=False,
+                                          time_to_live__gt=timezone.now(),
+                                          time_to_live__lt=time_week
+                                          ).count()
+    time_month = timezone.now() + timedelta(days=30)
+    files_count_month = File.objects.filter(time_to_live__isnull=False,
+                                            time_to_live__gt=timezone.now(),
+                                            time_to_live__lt=time_month
+                                            ).count()
+    time_year = timezone.now() + timedelta(days=364)
+    files_count_year = File.objects.filter(time_to_live__isnull=False,
+                                           time_to_live__gt=timezone.now(),
+                                           time_to_live__lt=time_year
+                                           ).count()
+
+    last_files = File.objects.all().order_by('-upload_date')[:2]
+
     return render(
         request,
         'index.html',
-        context={},
+        context={'files_count': files_count,
+                 'available_files_count': available_files_count,
+                 'user_count': user_count,
+                 'total_file_size': dc_total_file_size['total_file_size'],
+                 'files_count_hour': files_count_hour,
+                 'files_count_day': files_count_day,
+                 'files_count_week': files_count_week,
+                 'files_count_month': files_count_month,
+                 'files_count_year': files_count_year,
+                 'object_list': last_files,
+                 },
     )
 
 
@@ -154,3 +197,13 @@ class NewRandomFilesListView(generic.ListView):
             else:
                 return File.objects.filter(
                     Q(time_to_live__isnull=True) | Q(time_to_live__gt=timezone.now())).order_by('?')[:2]
+
+
+class NewLastFilesListView(generic.ListView):
+        model = File
+        template_name = 'common/new_random_files.html'
+
+        def get_queryset(self):
+            start_index = int(self.request.GET.get('index'))
+
+            return File.objects.all().order_by('-upload_date')[start_index:(start_index+2)]
