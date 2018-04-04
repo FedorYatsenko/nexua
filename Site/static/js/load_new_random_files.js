0eaf82558dc84d138e849452aa8e029c4f80e16a
FedorYatsenko <f.yatsenko@knu.ua>
2018-04-04 19:43:43 +0300

    Refactor code in nexua/Site/static/js/load_new_random_files.js

--- Site/static/js/load_new_random_files.js
+++ Site/static/js/load_new_random_files.js
@@ -1,31 +1,10 @@
 $(function () {
     "use strict";
 
-    $("#button_hour").click(function() {
+    $(".button_filter").click(function() {
         $(this).addClass('active').siblings().removeClass('active');
         $('#random_file_list').load(
-            $('#random_file_list').attr('url') + "?ttl=hour"
-        );
-    });
-
-    $("#button_day").click(function() {
-        $(this).addClass('active').siblings().removeClass('active');
-        $('#random_file_list').load(
-            $('#random_file_list').attr('url') + "?ttl=day"
-        );
-    });
-
-    $("#button_week").click(function() {
-        $(this).addClass('active').siblings().removeClass('active');
-        $('#random_file_list').load(
-            $('#random_file_list').attr('url') + "?ttl=week"
-        );
-    });
-
-    $("#button_all").click(function() {
-        $(this).addClass('active').siblings().removeClass('active');
-        $('#random_file_list').load(
-            $('#random_file_list').attr('url') + "?ttl=all"
+            $('#random_file_list').attr('url') + "?ttl=" + $(this).attr('name')
         );
     });
 });
