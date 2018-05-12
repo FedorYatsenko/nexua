e3c92382f5ae72730b9e7896289d58498ee11191
FedorYatsenko <f.yatsenko@knu.ua>
2018-05-12 10:41:42 +0300

    Change Site to lowercase(site)

--- nexua/settings.py
+++ nexua/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'site.apps.SiteConfig',
+    'site.apps.siteConfig',
 ]
 
 MIDDLEWARE = [
