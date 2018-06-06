067b10232838eee0b00c0379e3218fa0b1fd6b14
FedorYatsenko <f.yatsenko@knu.ua>
2018-06-06 17:01:07 +0300

    Add new random button

--- Site/static/js/load_new_random_files.js
+++ Site/static/js/load_new_random_files.js
@@ -2,9 +2,17 @@ $(function () {
     "use strict";
 
     $(".button_filter").click(function() {
-        $(this).addClass('active').siblings().removeClass('active');
+        if (!$(this).hasClass('active')) {
+            $(this).addClass('active').siblings().removeClass('active');
+            $('#random_file_list').load(
+                $('#random_file_list').attr('url') + "?ttl=" + $(this).attr('name')
+            );
+        }
+    });
+
+    $("#button_new").click(function() {
         $('#random_file_list').load(
-            $('#random_file_list').attr('url') + "?ttl=" + $(this).attr('name')
-        );
+                $('#random_file_list').attr('url') + "?ttl=" + $("#filters").children(".active").attr("name")
+            );
     });
 });
